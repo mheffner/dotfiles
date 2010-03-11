d65047c1eb340bac9883e6c36a14536729e5a4d2
Mike Heffner <mikeh@fesnel.com>
2010-03-11 09:31:00 -0500

    Add log aliases.

--- env/git_alias.sh
+++ env/git_alias.sh
@@ -29,6 +29,8 @@ galias ds diff --stat
 galias dfp diff
 galias lp log -p
 galias labbrev log --abbrev-commit --pretty=oneline
+galias lg log --oneline --decorate --graph
+galias lga log --oneline --decorate --graph --all
 
 # From: http://www.jukie.net/~bart/blog/pimping-out-git-log
 # galias lg log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative
